86a1e0b58722d4fa42aaa4b096f17b8992a5de63
PixelatedDino <yoshihutt@gmail.com>
2025-10-28 20:35:21 -0400

    April Tag and Progress on Pedro Tuning
    Implemented code to identify April Tags and restarted the tuning for pedro pathing.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/legacy/AprilTagTest.java
@@ -0,0 +1,120 @@
+package org.firstinspires.ftc.teamcode.legacy;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import java.util.List;
+import org.firstinspires.ftc.robotcore.external.JavaUtil;
+import org.firstinspires.ftc.robotcore.external.hardware.camera.BuiltinCameraDirection;
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.firstinspires.ftc.vision.VisionPortal;
+import org.firstinspires.ftc.vision.apriltag.AprilTagDetection;
+import org.firstinspires.ftc.vision.apriltag.AprilTagProcessor;
+
+@Autonomous(name = "AprilTagTest")
+public class AprilTagTest extends LinearOpMode {
+
+    boolean USE_WEBCAM;
+    AprilTagProcessor myAprilTagProcessor;
+    VisionPortal myVisionPortal;
+
+    /**
+     * This OpMode illustrates the basics of AprilTag recognition and pose estimation, using the easy way. All
+     * settings have default values. To customize settings with the Builder pattern, see the sample OpMode called
+     * ConceptAprilTag.
+     *
+     * For an introduction to AprilTags, see the FTC-DOCS link below:
+     * https://ftc-docs.firstinspires.org/en/latest/apriltag/vision_portal/apriltag_intro/apriltag-intro.html
+     *
+     * In this sample, any visible tag ID will be detected and
+     * displayed, but only tags that are included in the default
+     * "TagLibrary" will have their position and orientation information displayed. This default TagLibrary contains
+     * the current Season's AprilTags and a small set of "test Tags" in the high number range.
+     *
+     * When an AprilTag in the TagLibrary is detected, the SDK provides location and orientation of the tag, relative
+     * to the camera. This information is provided in the "ftcPose" member of the returned "detection", and is
+     * explained in the ftc-docs page linked below.
+     * https://ftc-docs.firstinspires.org/apriltag-detection-values
+     */
+
+    @Override
+    public void runOpMode() {
+        USE_WEBCAM = true;
+        // Initialize AprilTag before waitForStart.
+        initAprilTag();
+        // Wait for the match to begin.
+        telemetry.addData("DS preview on/off", "3 dots, Camera Stream");
+        telemetry.addData(">", "Touch START to start OpMode");
+        telemetry.update();
+        waitForStart();
+        if (opModeIsActive()) {
+            // Put run blocks here.
+            while (opModeIsActive()) {
+                // Put loop blocks here.
+                telemetryAprilTag();
+                // Push telemetry to the Driver Station.
+                telemetry.update();
+                if (gamepad1.dpad_down) {
+                    // Temporarily stop the streaming session. This can save CPU
+                    // resources, with the ability to resume quickly when needed.
+                    myVisionPortal.stopStreaming();
+                } else if (gamepad1.dpad_up) {
+                    // Resume the streaming session if previously stopped.
+                    myVisionPortal.resumeStreaming();
+                }
+                // Share the CPU.
+                sleep(20);
+            }
+        }
+    }
+
+
+    /**
+     * Initialize AprilTag Detection.
+     */
+    private void initAprilTag() {
+        // First, create an AprilTagProcessor.
+        myAprilTagProcessor = AprilTagProcessor.easyCreateWithDefaults();
+        // Next, create a VisionPortal.
+        if (USE_WEBCAM) {
+            myVisionPortal = VisionPortal.easyCreateWithDefaults(hardwareMap.get(WebcamName.class, "Webcam 1"), myAprilTagProcessor);
+        } else {
+            myVisionPortal = VisionPortal.easyCreateWithDefaults(BuiltinCameraDirection.BACK, myAprilTagProcessor);
+        }
+    }
+
+    /**
+     * Display info (using telemetry) for a recognized AprilTag.
+     */
+    private void telemetryAprilTag() {
+        List<AprilTagDetection> myAprilTagDetections;
+        AprilTagDetection myAprilTagDetection;
+
+        // Get a list of AprilTag detections.
+        myAprilTagDetections = myAprilTagProcessor.getDetections();
+        telemetry.addData("# AprilTags Detected", JavaUtil.listLength(myAprilTagDetections));
+        // Iterate through list and call a function to display info for each recognized AprilTag.
+        for (AprilTagDetection myAprilTagDetection_item : myAprilTagDetections) {
+            myAprilTagDetection = myAprilTagDetection_item;
+            // Display info about the detection.
+            telemetry.addLine("");
+            if (myAprilTagDetection.metadata != null) {
+                telemetry.addLine("==== (ID " + myAprilTagDetection.id + ") " + myAprilTagDetection.metadata.name);
+                telemetry.addLine("XYZ " + JavaUtil.formatNumber(myAprilTagDetection.ftcPose.x, 6, 1) + " " + JavaUtil.formatNumber(myAprilTagDetection.ftcPose.y, 6, 1) + " " + JavaUtil.formatNumber(myAprilTagDetection.ftcPose.z, 6, 1) + "  (inch)");
+                telemetry.addLine("PRY " + JavaUtil.formatNumber(myAprilTagDetection.ftcPose.pitch, 6, 1) + " " + JavaUtil.formatNumber(myAprilTagDetection.ftcPose.roll, 6, 1) + " " + JavaUtil.formatNumber(myAprilTagDetection.ftcPose.yaw, 6, 1) + "  (deg)");
+                telemetry.addLine("RBE " + JavaUtil.formatNumber(myAprilTagDetection.ftcPose.range, 6, 1) + " " + JavaUtil.formatNumber(myAprilTagDetection.ftcPose.bearing, 6, 1) + " " + JavaUtil.formatNumber(myAprilTagDetection.ftcPose.elevation, 6, 1) + "  (inch, deg, deg)");
+            } else {
+                telemetry.addLine("==== (ID " + myAprilTagDetection.id + ") Unknown");
+                telemetry.addLine("Center " + JavaUtil.formatNumber(myAprilTagDetection.center.x, 6, 0) + "" + JavaUtil.formatNumber(myAprilTagDetection.center.y, 6, 0) + " (pixels)");
+            }
+        }
+        telemetry.addLine("");
+        telemetry.addLine("key:");
+        telemetry.addLine("XYZ = X (Right), Y (Forward), Z (Up) dist.");
+        telemetry.addLine("PRY = Pitch, Roll & Yaw (XYZ Rotation)");
+        telemetry.addLine("RBE = Range, Bearing & Elevation");
+    }
+
+
+}
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/legacy/Decode_Red_Auto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/legacy/Decode_Red_Auto.java
@@ -9,47 +9,72 @@ import com.pedropathing.geometry.BezierCurve;
 import com.pedropathing.geometry.BezierLine;
 import com.pedropathing.util.Timer;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 
+
+import org.firstinspires.ftc.robotcore.external.JavaUtil;
+import org.firstinspires.ftc.robotcore.external.hardware.camera.BuiltinCameraDirection;
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
 import org.firstinspires.ftc.teamcode.pedroPathing.Constants;
+import org.firstinspires.ftc.vision.VisionPortal;
+import org.firstinspires.ftc.vision.apriltag.AprilTagDetection;
+import org.firstinspires.ftc.vision.apriltag.AprilTagProcessor;
 import org.opencv.core.Point;
 
+import java.util.List;
+
 import dev.nextftc.core.commands.Command;
+import dev.nextftc.core.commands.CommandManager;
 import dev.nextftc.core.commands.delays.Delay;
 import dev.nextftc.core.commands.groups.SequentialGroup;
 import dev.nextftc.extensions.pedro.FollowPath;
+import dev.nextftc.extensions.pedro.PedroComponent;
+import dev.nextftc.ftc.ActiveOpMode;
+import dev.nextftc.ftc.NextFTCOpMode;
+
+import static dev.nextftc.extensions.pedro.PedroComponent.follower;
+
 
 
 @Autonomous(name = "Decode Red Auto")
-public class Decode_Red_Auto extends OpMode {
+public class Decode_Red_Auto extends NextFTCOpMode {
+    public Decode_Red_Auto(){
+        addComponents(
+                new PedroComponent(Constants::createFollower)
+        );
+    }
 
-    private Follower follower;
-    private Timer pathTimer, actionTimer, opModeTimer;
-    private int pathState;
+
+//    private Follower follower;
+
     private final Pose startPose = new Pose(0, 0, 0);
-    private int counter = 0;
 
 
-    private Path scorePreload;
+    boolean USE_WEBCAM;
+    AprilTagProcessor myAprilTagProcessor;
+    VisionPortal myVisionPortal;
+
+
     private PathChain line1, line2, line3, line4;
 
     public void buildPaths() {
 
-               line1 = follower.pathBuilder()
-                .addPath(new BezierLine(startPose, new Pose(18, 0,0 )))
-                .setConstantHeadingInterpolation(startPose.getHeading())
+               line1 = follower().pathBuilder()
+                .addPath(new BezierLine(startPose, new Pose(24, 0,0 )))
+                .setConstantHeadingInterpolation(0)
                        .build();
-               line2 = follower.pathBuilder()
-                       .addPath(new BezierLine(new Pose(18,0,0), new Pose(18,18,0)))
-                       .setConstantHeadingInterpolation(startPose.getHeading())
+               line2 = follower().pathBuilder()
+                       .addPath(new BezierLine(new Pose(24,0,0), new Pose(24,24,0)))
+                       .setConstantHeadingInterpolation(0)
                        .build();
-               line3 = follower.pathBuilder()
-                       .addPath(new BezierLine(new Pose(18,18,0), new Pose(0,18,0))).
-                       setConstantHeadingInterpolation(startPose.getHeading())
+               line3 = follower().pathBuilder()
+                       .addPath(new BezierLine(new Pose(24,24,0), new Pose(0,24,0)))
+                       .setConstantHeadingInterpolation(0)
                        .build();
-               line4 = follower.pathBuilder()
-                       .addPath(new BezierLine(new Pose(0,18,0), new Pose(0,0,0))).
-                       setConstantHeadingInterpolation(startPose.getHeading())
+               line4 = follower().pathBuilder()
+                       .addPath(new BezierLine(new Pose(0,24,0), new Pose(0,0,0)))
+                       .setConstantHeadingInterpolation(0)
                        .build();
 
     }
@@ -70,45 +95,112 @@ public class Decode_Red_Auto extends OpMode {
 
     /** This is the main loop of the OpMode, it will run repeatedly after clicking "Play". **/
     @Override
-    public void loop() {
+    public void onUpdate() {
 
         // These loop the movements of the robot, these must be called continuously in order to work
-        follower.update();
-        doAuto();
+        follower().update();
 
         // Feedback to Driver Hub for debugging
-        telemetry.addData("path state", pathState);
-        telemetry.addData("x", follower.getPose().getX());
-        telemetry.addData("y", follower.getPose().getY());
-        telemetry.addData("heading", follower.getPose().getHeading());
+
+        telemetry.addData("x", follower().getPose().getX());
+        telemetry.addData("y", follower().getPose().getY());
+        telemetry.addData("heading", follower().getPose().getHeading());
         telemetry.update();
     }
 
     /** This method is called once at the init of the OpMode. **/
     @Override
-    public void init() {
-        pathTimer = new Timer();
+    public void onInit() {
 
 
-        follower = Constants.createFollower(hardwareMap);
+
+//        follower = Constants.createFollower(hardwareMap);
+//        follower.setStartingPose(startPose);
+        follower().setStartingPose(startPose);
         buildPaths();
-        follower.setStartingPose(startPose);
+
+
+        USE_WEBCAM = true;
+        // Initialize AprilTag before waitForStart.
+        initAprilTag();
+        // Wait for the match to begin.
+        telemetry.addData("DS preview on/off", "3 dots, Camera Stream");
+        telemetry.addData(">", "Touch START to start OpMode");
+        telemetry.update();
+
 
     }
 
     /** This method is called continuously after Init while waiting for "play". **/
     @Override
-    public void init_loop() {}
+    public void onWaitForStart() {
+        telemetryAprilTag();
+        // Push telemetry to the Driver Station.
+        telemetry.update();
+    }
 
     /** This method is called once at the start of the OpMode.
      * It runs all the setup actions, including building paths and starting the path system **/
     @Override
-    public void start() {
+    public void onStartButtonPressed() {
 
+
+        doAuto().schedule();
     }
 
     /** We do not use this because everything should automatically disable **/
     @Override
-    public void stop() {}
+    public void onStop() {
+
+    }
+
+    /**
+     * Initialize AprilTag Detection.
+     */
+    private void initAprilTag() {
+        // First, create an AprilTagProcessor.
+        myAprilTagProcessor = AprilTagProcessor.easyCreateWithDefaults();
+        // Next, create a VisionPortal.
+        if (USE_WEBCAM) {
+            myVisionPortal = VisionPortal.easyCreateWithDefaults(hardwareMap.get(WebcamName.class, "Webcam 1"), myAprilTagProcessor);
+        } else {
+            myVisionPortal = VisionPortal.easyCreateWithDefaults(BuiltinCameraDirection.BACK, myAprilTagProcessor);
+        }
+    }
+
+    /**
+     * Display info (using telemetry) for a recognized AprilTag.
+     */
+    private void telemetryAprilTag() {
+        List<AprilTagDetection> myAprilTagDetections;
+        AprilTagDetection myAprilTagDetection;
+
+        // Get a list of AprilTag detections.
+        myAprilTagDetections = myAprilTagProcessor.getDetections();
+        telemetry.addData("# AprilTags Detected", JavaUtil.listLength(myAprilTagDetections));
+        // Iterate through list and call a function to display info for each recognized AprilTag.
+        for (AprilTagDetection myAprilTagDetection_item : myAprilTagDetections) {
+            myAprilTagDetection = myAprilTagDetection_item;
+            // Display info about the detection.
+            telemetry.addLine("");
+            if (myAprilTagDetection.metadata != null) {
+                telemetry.addLine("==== (ID " + myAprilTagDetection.id + ") " + myAprilTagDetection.metadata.name);
+                telemetry.addLine("XYZ " + JavaUtil.formatNumber(myAprilTagDetection.ftcPose.x, 6, 1) + " " + JavaUtil.formatNumber(myAprilTagDetection.ftcPose.y, 6, 1) + " " + JavaUtil.formatNumber(myAprilTagDetection.ftcPose.z, 6, 1) + "  (inch)");
+                telemetry.addLine("PRY " + JavaUtil.formatNumber(myAprilTagDetection.ftcPose.pitch, 6, 1) + " " + JavaUtil.formatNumber(myAprilTagDetection.ftcPose.roll, 6, 1) + " " + JavaUtil.formatNumber(myAprilTagDetection.ftcPose.yaw, 6, 1) + "  (deg)");
+                telemetry.addLine("RBE " + JavaUtil.formatNumber(myAprilTagDetection.ftcPose.range, 6, 1) + " " + JavaUtil.formatNumber(myAprilTagDetection.ftcPose.bearing, 6, 1) + " " + JavaUtil.formatNumber(myAprilTagDetection.ftcPose.elevation, 6, 1) + "  (inch, deg, deg)");
+            } else {
+                telemetry.addLine("==== (ID " + myAprilTagDetection.id + ") Unknown");
+                telemetry.addLine("Center " + JavaUtil.formatNumber(myAprilTagDetection.center.x, 6, 0) + "" + JavaUtil.formatNumber(myAprilTagDetection.center.y, 6, 0) + " (pixels)");
+            }
+        }
+        telemetry.addLine("");
+        telemetry.addLine("key:");
+        telemetry.addLine("XYZ = X (Right), Y (Forward), Z (Up) dist.");
+        telemetry.addLine("PRY = Pitch, Roll & Yaw (XYZ Rotation)");
+        telemetry.addLine("RBE = Range, Bearing & Elevation");
+    }
+
+
+
 
 }
