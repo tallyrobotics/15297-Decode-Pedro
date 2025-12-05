96380775a2137b9f43eb67d4f5afe7c158481c6e
PixelatedDino <yoshihutt@gmail.com>
2025-12-05 00:31:35 -0500

    midnight check in...

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/legacy/subsystems/backLED.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/legacy/subsystems/backLED.java
@@ -2,10 +2,11 @@ package org.firstinspires.ftc.teamcode.legacy.subsystems;
 
 public class backLED extends LED {
 
-    public static final backLED INSTANCE = new backLED("ledBack");
+    public static final backLED INSTANCE = new backLED("ledBack", "colorBack", "colorBack");
 
-    public backLED(String ledName) {
-        super(ledName, "", "");
+
+    public backLED(String ledName, String colorName, String distanceName) {
+            super(ledName, colorName,distanceName);
     }
 
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/legacy/subsystems/rpmLED.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/legacy/subsystems/rpmLED.java
@@ -1,12 +1,79 @@
 package org.firstinspires.ftc.teamcode.legacy.subsystems;
 
-public class rpmLED extends LED {
+import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.opMode;
+import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.telemetry;
 
-    public static final rpmLED INSTANCE = new rpmLED("ledRPM");
+import com.qualcomm.robotcore.hardware.ColorSensor;
+import com.qualcomm.robotcore.hardware.DistanceSensor;
+import com.qualcomm.robotcore.hardware.NormalizedColorSensor;
+import com.qualcomm.robotcore.hardware.NormalizedRGBA;
 
-    public rpmLED(String ledName) {
-        super(ledName, "", "");
+import org.firstinspires.ftc.robotcore.external.JavaUtil;
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+
+import dev.nextftc.core.commands.Command;
+import dev.nextftc.core.subsystems.Subsystem;
+import dev.nextftc.hardware.impl.ServoEx;
+import dev.nextftc.hardware.positionable.SetPosition;
+
+public abstract class rpmLED implements Subsystem {
+
+
+    public rpmLED() {
+
+
     }
+
+
+    public ServoEx led;
+    private static final Double off = 0.0;
+    private static final Double red = 0.285;
+    private static final Double yellow = 0.41;
+    private static final Double green = 0.56;
+    private static final Double purple = 0.77;
+
+    public static String color;
+
+    public Command Off() {
+        color = "off";
+        return new SetPosition(led, off);
+    }
+
+    public Command Red() {
+        color = "red";
+        return new SetPosition(led, red);
+    }
+
+    public Command Yellow() {
+        color = "yellow";
+        return new SetPosition(led, yellow);
+    }
+
+    public Command Green() {
+        color = "green";
+        return new SetPosition(led, green);
+    }
+
+    public Command Purple() {
+        color = "purple";
+        return new SetPosition(led, purple);
+    }
+
+
+    @Override
+    public void initialize() {
+        led = new ServoEx("ledRPM");
+
+
+    }
+
+    @Override
+    public void periodic() {
+
+    }
+
+
+
 
 
 }
