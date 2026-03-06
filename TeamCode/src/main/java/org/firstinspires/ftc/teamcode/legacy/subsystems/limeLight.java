6fd34c14ab182c04b7dbee156aa1f46085a3dac3
PixelatedDino <yoshihutt@gmail.com>
2026-03-05 22:02:38 -0500

    Enabled Limelight For Now

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/legacy/subsystems/limeLight.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/legacy/subsystems/limeLight.java
@@ -12,6 +12,7 @@ import java.util.List;
 import dev.nextftc.core.commands.Command;
 import dev.nextftc.core.commands.utility.InstantCommand;
 import dev.nextftc.core.subsystems.Subsystem;
+import dev.nextftc.ftc.ActiveOpMode;
 
 public class limeLight implements Subsystem {
 
@@ -20,51 +21,58 @@ public class limeLight implements Subsystem {
 
     }
 
-//    public Limelight3A limelight;
-//    String limename = "limelight";
-//    Integer balls = 0;
-//    Integer finalBalls = 0;
-//
-//
-//
-//    @Override
-//    public void initialize()
-//    {
-//        limelight = hardwareMap.get(Limelight3A.class, limename);
-//        limelight.setPollRateHz(100); // This sets how often we ask Limelight for data (100 times per second)
-//        limelight.pipelineSwitch(0);
-//    }
-//
-//    public Command On(){
-//        return new InstantCommand(()-> {limelight.start();});
-//    }
-//    public Command Off(){
-//        return new InstantCommand(()-> {limelight.stop();});
-//    }
-//    public Integer GetBalls(){
-//        return finalBalls;
-//    }
-//
-//
-//    @Override
-//    public void periodic(){
-//        if(limelight.isRunning()){
-//            LLResult result = limelight.getLatestResult();
-//            if (result != null && result.isValid()) {
-//
-//                List<LLResultTypes.DetectorResult> detections = result.getDetectorResults();
-//                for (LLResultTypes.DetectorResult detection : detections) {
-//                    balls++;
-//                }
-//
-//            } else {
-//                telemetry.addData("Limelight", "No Targets");
-//            }
-//        }
-//
-//        finalBalls = balls;
-//        telemetry.addData("Balls", balls);
-//        telemetry.update();
-//        balls = 0;
-//    }
+    public Limelight3A limelight;
+    String limename = "limelight";
+    Integer balls = 0;
+    Integer finalBalls = 0;
+
+
+
+    @Override
+    public void initialize()
+    {
+        limelight = ActiveOpMode.hardwareMap().get(Limelight3A.class, limename);
+        limelight.setPollRateHz(100); // This sets how often we ask Limelight for data (100 times per second)
+        limelight.pipelineSwitch(0);
+    }
+
+    public Command On0(){
+        return new InstantCommand(()-> {limelight.start();
+        limelight.pipelineSwitch(0);});
+    }
+
+    public Command On1(){
+        return new InstantCommand(()-> {limelight.start();
+        limelight.pipelineSwitch(1);});
+    }
+
+
+    public Command Off(){
+        return new InstantCommand(()-> {limelight.stop();});
+    }
+    public Integer GetBalls(){
+        return finalBalls;
+    }
+
+
+    @Override
+    public void periodic(){
+        if(limelight.isRunning()){
+            LLResult result = limelight.getLatestResult();
+            if (result != null && result.isValid()) {
+
+                List<LLResultTypes.DetectorResult> detections = result.getDetectorResults();
+                for (LLResultTypes.DetectorResult detection : detections) {
+                    balls++;
+                }
+
+            } else {
+                ActiveOpMode.telemetry().addData("Limelight", "No Targets");
+            }
+        }
+
+        finalBalls = balls;
+        ActiveOpMode.telemetry().addData("Balls", balls);
+        balls = 0;
+    }
 }
